4768bbdc3902332705fa103ea4377a9b0341dde3
mohamedtamer0 <mt4452242@gmail.com>
2024-03-01 11:36:00 +0200

    junit

--- 1.00-starting-project/src/test/java/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,43 @@
 package junitdemo;
 
 import org.example.DemoUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void setupAfterEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@AfterEach");
+        System.out.println();
+    }
+
+
+    @BeforeAll
+    static void setupBeforeAll() {
+        System.out.println("@BeforeAll");
+        System.out.println();
+    }
+
+    @AfterAll
+    static void setupAfterAll() {
+        System.out.println("@AfterAll");
+    }
+
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test : testEqualsAndNotEquals");
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 4), "2+4 must not be 6");
 
@@ -16,7 +45,7 @@ public class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test : testEqualsAndNotEquals");
         String str1 = null;
         String str2 = "Tamer";
 
